446a3708be39c1e192d4473fbc32e01c7d260356
Avijit Ghosh <81845097+avighoshjit567@users.noreply.github.com>
2025-09-14 21:30:22 +0600

    feat : default route login added

--- frontend/src/components/AppRoutes.jsx
+++ frontend/src/components/AppRoutes.jsx
@@ -16,8 +16,8 @@ function AppRoutes() {
     return (
         <Router>
             <Routes>
+                <Route path="/" element={<Navigate to="/login" replace />} />
                 <Route path="/login" element={<Login />} />
-                
                 <Route path="/servers" element={
                     <ProtectedRoute>
                         <Servers />
